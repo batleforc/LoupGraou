5f3224b33988b676bdcdfe7f3724ad0ff469784d
Maxime Leriche <maxleriche.60@gmail.com>
2021-11-21 18:42:56 +0100

    [add] No User But Player

---
+++ Moteur/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moteur
+{
+    public class Player
+    {
+        private string username;
+        private string socketId;
+        private bool IsAlive;
+        private IRole role;
+        private IRole[] subRole;
+        private string userPic;
+
+    }
+}
--- Moteur/User.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Moteur
-{
-    public class User
-    {
-
-    }
-}
