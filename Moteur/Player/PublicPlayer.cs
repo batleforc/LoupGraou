55f4b0a3582db313c8c1c1f0f16d1554402b952d
Maxime Leriche <maxleriche.60@gmail.com>
2021-11-21 18:43:37 +0100

    [add] Public Player and role

---
+++ Moteur/Player/PublicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moteur
+{
+    public class PublicPlayer
+    {
+        string username;
+        string userPic;
+        bool isAlive;
+        IRole? role;
+    }
+}
---
+++ Moteur/Role/IRole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moteur
+{
+    public interface IRole
+    {
+        int DailyVotePoder();
+        int NightVotePoder();
+        int NightOrder();
+        void NightAction();
+        void Die();
+        void Victory();
+        bool IsSubRole();
+    }
+}
